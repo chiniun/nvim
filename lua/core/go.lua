b3a69e923c7d0e9265ee55ae451a393dde43c1a6
武东东 <chiniun@outlook.com>
2023-12-21 16:46:54 +0800

    初始化

---
+++ lua/core/go.lua
@@ -0,0 +1,13 @@
+vim.g.encoding = "UTF-8"
+vim.o.fileencoding = "UTF-8"
+
+-- 开启 Go 代码的自动补全
+vim.g.go_completion_enabled = 1
+
+-- 在格式化代码时静默处理错误
+vim.g.go_fmt_fail_silently = 1
+
+-- 启用 Go 代码的静态分析工具 'vet'
+vim.g.go_metalinter_enabled = {'vet'}
+
+
